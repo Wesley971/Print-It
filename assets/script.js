fbeea60fb9cbb8d1008416e6a98bbb5d00661322
Wesley Abdoul <Abdoulwes@gmail.com>
2023-12-12 17:11:16 +0100

    update

--- assets/script.js
+++ assets/script.js
@@ -1,84 +1,80 @@
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
-
+  {
+    image: "slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
 
 // Variable
 
-const dots = document.querySelector(".dots")
-const arrowRight = document.querySelector(".arrow_right")
-const arrowLeft = document.querySelector(".arrow_left")
-const img = document.querySelector("#banner .banner-img") 
-const p = document.querySelector("#banner p")
+const dots = document.querySelector(".dots");
+const arrowRight = document.querySelector(".arrow_right");
+const arrowLeft = document.querySelector(".arrow_left");
+const img = document.querySelector("#banner .banner-img");
+const p = document.querySelector("#banner p");
 
-let index = 0
+let index = 0;
 
 // Boucles
 
-function displayDots(){
-	for (let i = 0; i < slides.length; i++){
-		const dot = document.createElement("div")
-		dot.classList.add("dot")
-		dots.appendChild(dot)
-		if (i == index) {
-			dot.classList.add("dot_selected")
-		}
-	}
+function displayDots() {
+  for (let i = 0; i < slides.length; i++) {
+    const dot = document.createElement("div");
+    dot.classList.add("dot");
+    dots.appendChild(dot);
+    if (i == index) {
+      dot.classList.add("dot_selected");
+    }
+  }
 }
-displayDots()
+displayDots();
 
 // Fonction clique droit
 
-function clickRight(){
-	arrowRight.addEventListener("click", () => {
-		const slidesDots = document.querySelectorAll(".dots .dot")
-		slidesDots[index].classList.remove("dot_selected")
+function clickRight() {
+  arrowRight.addEventListener("click", () => {
+    const slidesDots = document.querySelectorAll(".dots .dot");
+    slidesDots[index].classList.remove("dot_selected");
 
-		
-		console.log("Vers la droite");
-		index++;
-		if (index >= slides.length){
-			index = 0
-		}
-		slidesDots[index].classList.add("dot_selected")
-		img.src = `./assets/images/slideshow/${slides[index].image}`;
-		p.innerHTML = slides[index].tagLine
-		
-	} )
-
+    console.log("Vers la droite");
+    index++;
+    if (index >= slides.length) {
+      index = 0;
+    }
+    slidesDots[index].classList.add("dot_selected");
+    img.src = `./assets/images/slideshow/${slides[index].image}`;
+    p.innerHTML = slides[index].tagLine;
+  });
 }
+clickRight();
 
-clickRight()
-//Focntion clique gauche
-function clickLeft(){
-	arrowLeft.addEventListener("click", () => {
-		const slidesDots = document.querySelectorAll(".dots .dot")
-		slidesDots[index].classList.remove("dot_selected")
-		console.log("Vers la gauche");
-		index--
-		if(index < 0){
-			index = slides.length -1;
-		}
-		slidesDots[index].classList.add("dot_selected")
-		img.src = `./assets/images/slideshow/${slides[index].image}`;
-		p.innerHTML = slides[index].tagLine
-	}
-	)
+//Fonction clique gauche
+
+function clickLeft() {
+  arrowLeft.addEventListener("click", () => {
+    const slidesDots = document.querySelectorAll(".dots .dot");
+    slidesDots[index].classList.remove("dot_selected");
+    console.log("Vers la gauche");
+    index--;
+    if (index < 0) {
+      index = slides.length - 1;
+    }
+    slidesDots[index].classList.add("dot_selected");
+    img.src = `./assets/images/slideshow/${slides[index].image}`;
+    p.innerHTML = slides[index].tagLine;
+  });
 }
-clickLeft()
-
+clickLeft();
